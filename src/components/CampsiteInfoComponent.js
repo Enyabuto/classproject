d11e3607590b5e3777d2e4b785c79bf707e78679
Enyabuto <elishahyperx@yahoo.com>
2020-12-05 11:57:15 -0600

    Form valdation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
 import React from 'react'; 
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader} from 'reactstrap';
 import { Link } from 'react-router-dom';    
+import ModalBody from 'reactstrap/lib/ModalBody';
+import { Control, Errors, LocalForm } from 'react-redux-form';
     function RenderCampsite({campsite}){
         return(
             <div className="col-md-5 m-1">
@@ -12,6 +14,93 @@ import { Link } from 'react-router-dom';
                 </Card>
             </div>
         );
+    }
+
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+    class CommentForm extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state= {
+                isModalOpen: false,
+            };
+            this.toggleModal = this.toggleModal.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+
+        toggleModal() {
+            this.setState({ isModalOpen: !this.state.isModalOpen });
+        }
+
+        handleSubmit(values) {
+            console.log('Current state is: ' + JSON.stringify(values));
+            alert('Current state is: ' + JSON.stringify(values));
+        }
+
+
+        render() {
+            return(
+                <React.Fragment>
+                    <Button onClick={this.toggleModal} className="fa fa-pencil" outline>
+                        Submit Comment
+                    </Button>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="rating">Rating</label>
+                                    <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="author">Your Name</label>
+                                    <Control.text
+                                         model=".author"
+                                         id="author"
+                                         name="author"
+                                         className="form-control"
+                                         placeholder="author"
+                                         validators={{
+                                             minLength: minLength(2),
+                                             maxLength: maxLength(15),
+                                         }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: "Must be at least 2 characters",
+                                            maxLength: "Must be 15 characters or less",
+                                        }}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="comment">Comment</label>
+                                    <Control.textarea
+                                        model=".comment"
+                                        id="comment"
+                                        name="comment"
+                                        className="form-control"
+                                        placeholder="Comment"
+                                        rows="6"
+                                    />
+                                </div>
+                                <Button type="submit" color="primary">Submit</Button>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
+                </React.Fragment>
+            );
+        }
     }
     
     function RenderComments({comments}){
@@ -28,6 +117,7 @@ import { Link } from 'react-router-dom';
                     </div>
                 );
             })}
+            <CommentForm />
             </div>
         )
         return <div/>;
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -36,9 +36,6 @@ class Contact extends Component {
     }
 
 
-
-    
-
     handleSubmit(values) {
         console.log('Current state is: ' + JSON.stringify(values));
         alert('Current state is: ' + JSON.stringify(values));
